d923e496807e072e665e3ba3082db52a9ba7edcb
Aweroro Hadi <hadistic20@gmail.com>
2024-11-12 19:48:59 +0100

    refactor(auth-screens): updated auth screen design

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,5 @@
-"use client"
-import React from 'react'
+"use client";
+import React from 'react';
 import AuthHeader from '@/app/components/page-compoents/AuthHeader';
 import GoogleIcon from '@/app/components/ui-components/icons/GoogleIcon';
 import PrimaryButton from '@/app/components/ui-components/Buttons/PrimaryButton';
@@ -12,35 +12,36 @@ const SignUpPage = () => {
     <div className='flex container mx-auto max-w-xl'>
         <AuthHeader/>
       <div className='relative container mt-20 min-h-[80vh]'>
-        <h1 className='text-3xl font-bold ml-24 mt-16'>
+        <h1 className='text-xl md:text-3xl lg:text-4xl font-bold ml-24 lg:ml-2 mt-16 md:mt-24 lg:mt-28'>
             Sign up
         </h1>
         <div className='flex flex-col items-center mt-10 max-w-xl'>
-            <button className='flex items-center gap-x-2 font-bold border px-16 py-3 rounded-md shadow-sm hover:bg-gray-100'>
-                <GoogleIcon className='size-5'/>
+            <button className='flex items-center gap-x-2 lg:gap-x-4 font-bold text-base lg:text-2xl border px-16 px-28 lg:px-32 py-3 lg:py-4 rounded-md shadow-sm hover:bg-gray-100'>
+                <GoogleIcon className='size-5 lg:size-7'/>
                 Continue with Google
             </button>
             <form className='flex flex-col justify-start mt-6 space-y-4'>
                 <input
                 type='email'
-                className='border w-[320px] rounded-md shadow pl-2 py-3 hover:ring-2 hover:ring-[#6366F1] focus:outline-none focus:ring-2 focus:ring-[#6366F1] transition duration-150'
+                className='border w-[420px] lg:w-[550px] rounded-md shadow pl-2 py-3 hover:ring-2 hover:ring-[#6366F1] focus:outline-none focus:ring-2 focus:ring-[#6366F1] transition duration-150'
                 placeholder='Enter your email...'
                 />
                 <input
                 type='password'
-                className='border px-2 rounded-md shadow pl-2 py-3 hover:ring-2 hover:ring-[#6366F1] focus:outline-none focus:ring-2 focus:ring-[#6366F1] transition duration-150'
+                className='border w-[420px] lg:w-[550px] px-2 rounded-md shadow pl-2 py-3 hover:ring-2 hover:ring-[#6366F1] focus:outline-none focus:ring-2 focus:ring-[#6366F1] transition duration-150'
                 placeholder='Enter your password...'
                 />
                 <PrimaryButton
+                className='text-base lg:text-2xl'
                 title='Sign up with Email'
                 />
             </form>
-            <div className='flex space-x-2 text-sm font-light mt-10 text-gray-700'>
-                <p>Already signed up?</p>
+            <div className='flex space-x-2 text-sm lg:text-lg font-light mt-10 text-gray-700'>
+                <p>Already have an account?</p>
                 <button
                 onClick={() => router.push('/login')}
                 className='underline'>
-                    Go to login
+                    Log in
                 </button>
             </div>
         </div>
--- src/app/components/page-compoents/AuthHeader.tsx
+++ src/app/components/page-compoents/AuthHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const AuthHeader = () => {
     return (
-      <nav className='flex justify-between bg-white overflow-hidden p-8 py-2 fixed inset-x-20 top-2 z-10 '>
+      <nav className='flex justify-between bg-white overflow-hidden p-8 py-2 fixed inset-x-20 top-0 z-10 '>
            <header 
             className='lg:text-2xl sm:text-xl md:text-2xl text-[#6366F1] font-bold cursor-pointer font-serif mx-4'>
                Booktroll 
